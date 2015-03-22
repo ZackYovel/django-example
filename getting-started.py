b4517db28a16a2ef8fba6642687629baad06acef
zack <yovel.zack@gmail.com>
2015-03-22 03:31:58 +0200

    replaced wsgihandler with get_wsgi_application as instructed here: https://code.djangoproject.com/ticket/23146

--- getting-started.py
+++ getting-started.py
@@ -14,10 +14,12 @@ changing any of it's tag letters (# GETTING-STARTED:) or just delete it.
 
 class GettingStartedManager:
     FORMAT = "# GETTING-STARTED:"
+    changes = {}
 
-
-    def __init__(self, change=False, hidden=False, select_all=False, hide_all=False, unhide_all=False,
-            remove_un_hidden=False, remove_hidden=False, remove_all=False paths=('.'), prefix='#'):
+    def __init__(self, change=False, hidden=False, select_all=False,
+                 hide_all=False, unhide_all=False,
+                 remove_un_hidden=False, remove_hidden=False, remove_all=False,
+                 paths=('.'), prefix='#'):
         """Create a new GettingStartedManager, targeting the given path."""
         self._change = change
         self._hidden = hidden
@@ -30,30 +32,83 @@ class GettingStartedManager:
 
         # general cases
         self._select_all = select_all or remove_all
-        self._hidden_args = hidden or unhide_all or remove_hidden
+        self._hidden_mode = hidden or unhide_all or remove_hidden
 
         # formats to use (hidden and unhidden)
-        self._formats = (
-            self.FORMAT if select_all or not hidden_args,
-            prefix + self.FORMAT if select_all or hidden_args
-        )
+        self._formats = []
+        if select_all or not self._hidden_mode:
+            self._formats.append(self.FORMAT)
+        if select_all or self._hidden_mode:
+            self._formats.append(prefix + self.FORMAT)
+
+        # change mode: is this GSM allowed to write changes.
+        self._change_mode = any((
+            self._change, self._hide_all, self._unhide_all,
+            self._remove_un_hidden,
+            self._remove_hidden, self._remove_all
+        ))
+
+        self._batch_mode = any((
+            self._select_all, self._hide_all, self._unhide_all,
+            self._remove_hidden,
+            self._remove_un_hidden
+        ))
+
+    def is_hidden(self, comment):
+        return not comment.startswith(self.FORMAT)
+
+    @property
+    def hidden_mode(self):
+        return self._hidden_mode
+
+    @property
+    def change_mode(self):
+        return self._change_mode
+
+    @property
+    def batch_mode(self):
+        return self._batch_mode
+
+    def set_change(self, file_name, line_number, new_line):
+        if self._change_mode:
+            if file_name not in self.changes:
+                self.changes[file_name] = []
+            self.changes[file_name].append((line_number, new_line))
+
+    def comments(self):
+        # for every file in every path
+        for path in self._paths:
+            if os.path.isdir(path):
+                for root, f in (files for root, s, files in os.walk(path)):
+                    return self.process_file(root, f)
+            else:
+                return self.process_file('', path)
 
 
-    def search(self):
-        for f in (root, s, files in os.walk(path) for path in self._paths):
-            line_counter = 1
-            relative_file = os.path.join(root, f)
-            for line in open(relative_file):
-                line = line.strip()
-                if line.startswith(fmt for fmt in self._formats):
-                    yield (relative_file, line_counter, line)
-                line_counter += 1
+    def write(self):
+        if self._change_mode:
+            for file_name in self.changes:
+                with open(file_name) as f:
+                    lines = f.readlines()
+                    for line_number, new_line in self.changes[file_name]:
+                        lines[line_number] = new_line + "/n"
+                with open(file_name, 'w') as f:
+                    f.writelines(lines)
+
+    def process_file(self, root, f):
+        line_counter = 1
+        relative_file = os.path.join(root, f)
+        for line in open(relative_file):
+            line = line.strip()
+            if (line.startswith(fmt) for fmt in self._formats):
+                yield (relative_file, line_counter, line)
+            line_counter += 1
 
 
 if __name__ == "__main__":
     from sys import argv
 
-    USAGE ="""
+    USAGE = """
 usage: getting-started.py [arg1[ arg2]]
 
 options:
@@ -85,77 +140,100 @@ options:
         the -h or --hidden flags are present)
 """
 
-    change=False
-    hidden=False
-    select_all=False
-    hide_all=False
-    unhide_all=False,
-    # remove_un_hidden=False
-    # remove_hidden=False
-    remove_all=False
-    # paths=('.')
-    # prefix='#'
+    change = False
+    hidden = False
+    select_all = False
+    hide_all = False
+    unhide_all = False
+    remove_un_hidden = False
+    remove_hidden = False
+    remove_all = False
+    paths = ('.',)
+    prefix = '#'
 
-    for arg in argv:
-        if arg.endswith('getting-started.py'):
-            pass
-        elif arg in ('-ra', '--remove-all'):
-            remove_all = True
-        elif arg in ('-ha', '--hide-all'):
-            hide_all = True
-        elif arg in ('-ua', '--unhide-all'):
-            unhide_all = True
-        elif arg in ('-sa', '--select_all'):
-            select_all = True
-        elif arg in ('-h', '--hidden'):
-            hidden = True
-        elif arg in ('-c','--chage'):
-            change = True
-        else:
-            print USAGE
-            exit(0)
+    arg = None
+    try:
+        for arg in argv:
+            if arg is None or arg.endswith('getting-started.py'):
+                pass
+            elif arg in {'-pr', '--prefix'}:
+                i = argv.index(arg) + 1
+                prefix = argv[i]
+                argv[i] = None
+            elif arg in {'-p', '--paths'}:
+                i = argv.index(arg) + 1
+                paths = argv[i].split(";")
+                argv[i] = None
+            elif arg in ('-ra', '--remove-all'):
+                remove_all = True
+            elif arg in ('-ha', '--hide-all'):
+                hide_all = True
+            elif arg in ('-rmh', '--remove-hidden'):
+                remove_hidden = True
+            elif arg in ('-rmuh', '--remove-unhidden'):
+                remove_un_hidden = True
+            elif arg in ('-ua', '--unhide-all'):
+                unhide_all = True
+            elif arg in ('-sa', '--select_all'):
+                select_all = True
+            elif arg in ('-h', '--hidden'):
+                hidden = True
+            elif arg in ('-c', '--chage'):
+                change = True
+            else:
+                print USAGE
+                exit(0)
+    except IndexError as ie:
+        if arg in {'-pr', '--prefix'} and paths == ('.',):
+            print """
+usage:
+    getting-started.py [arg1[arg2[...]]] -pr <str> [more_arg1[more_arg2[...]]]
 
-    # all_mode = remove_all or hide_all or unhide_all
+A prefix token must be sent as the next argument.
+"""
+        if arg in {'-p', '--paths'} and prefix == '#':
+            print """
+usage:
+    getting-started.py [arg1[arg2[...]]] -p <;-separated-list> [more_arg1[more_arg2[...]]]
 
-    # # (un)hide-all=True impicitly means change_mode=True, but not the other way.
-    # if all_mode:
-    #     change_mode = True
+A smi-colon separated list of paths mush be sent as the next argument.
+"""
+        exit(1)
 
-    # files = {}
+    gsm = GettingStartedManager(
+        change=change, hidden=hidden, select_all=select_all, hide_all=hide_all,
+        unhide_all=unhide_all, remove_un_hidden=remove_un_hidden,
+        remove_hidden=remove_hidden, remove_all=remove_all, paths=paths,
+        prefix=prefix
+    )
 
-    # prefix = '#' if hidden_mode or unhide_all else ''
-    # for relative_file, line_number, line in find_getting_started_comments('.', prefix):
-    #     if not all_mode:
-    #         print(relative_file + " line " + str(line_number) + ":\n" + line)
+    if not gsm.batch_mode:
+        for relative_file, line_number, line in gsm.comments():
+            print(relative_file + " line " + str(line_number) + ":\n" + line)
+            if gsm.change_mode:
+                if gsm.is_hidden(line):
+                    action = raw_input("skip/unhide/remove? [skip]")
+                else:
+                    action = raw_input("skip/hide/remove? [skip]:")
+            else:
+                action = raw_input("next?")
 
-    #     if not all_mode:
-    #         if change_mode:
-    #             if hidden_mode:
-    #                 action = raw_input("skip/un-hide/remove? [skip]:")
-    #             else:
-    #                 action = raw_input("skip/hide/remove? [skip]:")
-    #         else:
-    #             action = raw_input("next?")
+            if gsm.change_mode:
+                if not relative_file in gsm.changes:
+                    gsm.changes[relative_file] = []
+                if unhide_all or not gsm.batch_mode and action in {'u',
+                                                                   'un-hide',
+                                                                   'unhide',
+                                                                   "un hide"}:
+                    change = line[1:]
+                elif hide_all or not gsm.batch_mode and action in {'hide',
+                                                                   'h'}:
+                    change = '#' + line
+                elif remove_all or not gsm.batch_mode and action in {'r',
+                                                                     'remove'}:
+                    change = ''
+                else:
+                    print "unknowen action: " + action
+                gsm.set_change(relative_file, line_number, change)
 
-    #     if change_mode:
-    #         if not relative_file in files:
-    #             files[relative_file] = []
-    #         if unhide_all or not all_mode and action in {'u', 'un-hide', 'unhide', "un hide"}:
-    #             change = line[1:]
-    #         elif hide_all or not all_mode and action in {'hide', 'h'}:
-    #             change = '#' + line
-    #         elif remove_all or not all_mode and action in {'r', 'remove'}:
-    #             change = ''
-    #         else:
-    #             print "unknowen action: " + action
-    #         files[relative_file] += [(line_number, change)]
-
-    # if change_mode:
-    #     for path in files:
-    #         changes = files[path]
-    #         with open(path) as f:
-    #             lines = f.readlines()
-    #             for change in changes:
-    #                 lines[change[0]-1] = change[1] + "\n"
-    #         with open(path, 'w') as f:
-    #             f.writelines(lines)
+    gsm.write()
